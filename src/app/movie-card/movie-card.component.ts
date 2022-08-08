ab99299b39672d24e0c517184b2f85ff90b4f035
Coldest-Out <98006784+Coldest-Out@users.noreply.github.com>
2022-08-07 22:41:22 -0700

    6.4 Moch-up

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -1,17 +1,19 @@
 // src/app/movie-card/movie-card.component.ts
 import { Component, OnInit } from '@angular/core';
-import { FetchApiDataService } from '../fetch-api-data.service';
-
+import { FetchApiDataService } from '../fetch-api-data.service'
+//Material design
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-movie-card',
   templateUrl: './movie-card.component.html',
-  //styleUrls: ['./movie-card.component.css']
+  styleUrls: ['./movie-card.component.scss']
 })
 export class MovieCardComponent implements OnInit {
   movies: any[] = [];
+  favorites: any[] = [];
+
 
   constructor(
     public fetchApiData: FetchApiDataService,
@@ -20,11 +22,15 @@ export class MovieCardComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+    this.getMovies();
+
+  }
+
+  getMovies(): void {
     this.fetchApiData.getAllMovies().subscribe((resp: any) => {
       this.movies = resp;
       console.log(this.movies);
       return this.movies;
     });
   }
-
 }
--- src/app/welcome-page/welcome-page.component.ts
+++ src/app/welcome-page/welcome-page.component.ts
@@ -1,31 +1,23 @@
+
 import { Component, OnInit } from '@angular/core';
 import { UserLoginFormComponent } from '../user-login-form/user-login-form.component';
 import { UserRegistrationFormComponent } from '../user-registration-form/user-registration-form.component';
-
 import { MatDialog } from '@angular/material/dialog';
-import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-welcome-page',
   templateUrl: './welcome-page.component.html',
-  styleUrls: ['./welcome-page.component.css']
+  styleUrls: ['./welcome-page.component.scss']
 })
 export class WelcomePageComponent implements OnInit {
-
-  constructor(
-    public dialog: MatDialog,
-    public snackBar: MatSnackBar
-  ) { }
-
+  constructor(public dialog: MatDialog) { }
   ngOnInit(): void {
   }
-
   openUserRegistrationDialog(): void {
     this.dialog.open(UserRegistrationFormComponent, {
       width: '280px'
     });
   }
-
   openUserLoginDialog(): void {
     this.dialog.open(UserLoginFormComponent, {
       width: '280px'
